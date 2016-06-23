601482c7ffc109aff2c6175b135f1768b54a8876
berziiii <brian@offpistedesigns.com>
2016-06-23 15:26:01 -0400

    Scaffold programs

---
+++ app/controllers/programs_controller.rb
@@ -0,0 +1,59 @@
+class ProgramsController < ApplicationController
+  before_action :set_program, only: [:show, :update, :destroy]
+
+  # GET /programs
+  # GET /programs.json
+  def index
+    @programs = Program.all
+
+    render json: @programs
+  end
+
+  # GET /programs/1
+  # GET /programs/1.json
+  def show
+    render json: @program
+  end
+
+  # POST /programs
+  # POST /programs.json
+  def create
+    @program = Program.new(program_params)
+
+    if @program.save
+      render json: @program, status: :created, location: @program
+    else
+      render json: @program.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /programs/1
+  # PATCH/PUT /programs/1.json
+  def update
+    @program = Program.find(params[:id])
+
+    if @program.update(program_params)
+      head :no_content
+    else
+      render json: @program.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /programs/1
+  # DELETE /programs/1.json
+  def destroy
+    @program.destroy
+
+    head :no_content
+  end
+
+  private
+
+    def set_program
+      @program = Program.find(params[:id])
+    end
+
+    def program_params
+      params.require(:program).permit(:title)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  resources :examples, except: [:new, :edit]
+
+  # Program Routes
+  resources :programs, except: [:new, :edit]
+
+  # User Routes
+  resources :users, only: [:index, :show]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
-  resources :users, only: [:index, :show]
+
+  # Example Routes
+  resources :examples, except: [:new, :edit]
 end
